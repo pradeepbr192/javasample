f00e66fca829d6fea35da6b566f866db074d8a78
pradeep <pradeepbr192@gmail.com>
2021-07-11 13:05:17 +0530

    modified code

--- src/main/java/Maventest/Maventest/Democlass.java
+++ src/main/java/Maventest/Maventest/Democlass.java
@@ -1,19 +1,22 @@
 package Maventest.Maventest;
 
-import org.testng.annotations.Test;
 
-import junit.framework.Assert;
+//import org.testng.annotations.Test;
+
+//import junit.framework.Assert;
 
 public class Democlass {
 	
 	
-	@Test
+	//@Test
 public void sum()
 {
 	System.out.println("Sum");
 	int a = 10;
 	int b = 20;
-	Assert.assertEquals(30, a +b );
+	int c = 0 ;
+	//Assert.assertEquals(30, a +b );
+	System.out.println(c);
 }
 
 }
